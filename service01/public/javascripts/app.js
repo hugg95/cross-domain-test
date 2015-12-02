fb650818e9f0bb413205fb2aeea99f77bbdbc99f
linianchao <linianchao@meituan.com>
2015-12-02 23:02:56 +0800

    add cors

---
+++ service01/public/javascripts/app.js
@@ -0,0 +1,6 @@
+console.log('hello, this is service01');
+
+var body = document.querySelector('body');
+var hello = document.createElement('h2');
+hello.innerText = 'Hello';
+body.appendChild(hello);
--- service02/public/javascripts/app.js
+++ service02/public/javascripts/app.js
@@ -4,6 +4,7 @@
  * send ajax request to test1.iframe.com:8888
  */
 $(function() {
+    /*
     $.get('http://test1.iframe.com:8888/test', function(data) {
             console.log(data);
         });
@@ -11,5 +12,21 @@ $(function() {
     $.get('http://test1.iframe.com:8888/test/jsonp', function(data) {
             console.log(data);
         });
+    */
+
+    $.ajax({
+        url: 'http://test1.iframe.com:8888/test',
+        type: 'POST',
+        beforeSend: function(request) {
+            request.setRequestHeader('MY-NAME', 'Victor');
+            request.setRequestHeader('Content-Type', 'application/xml');
+        },
+        success: function(data) {
+            console.log(data);
+        },
+        error: function(error) {
+            console.log(error);
+        }
+    });
 });
 
